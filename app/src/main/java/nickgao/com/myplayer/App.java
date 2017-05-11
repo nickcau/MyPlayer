3732fa0d628022712eb848ab2d2bc8e404d410a7
高友健 <youjiancau@163.com>
2017-05-11 17:11:30 +0800

    commit

---
+++ app/src/main/java/nickgao/com/myplayer/App.java
@@ -0,0 +1,25 @@
+package nickgao.com.myplayer;
+
+import android.app.Application;
+
+import com.meetyou.media.player.client.MeetyouPlayerEngine;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Linhh on 16/12/27.
+ */
+
+public class App extends Application{
+
+    public static OkHttpClient mHttpClient;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mHttpClient = new OkHttpClient.Builder().build();
+        MeetyouPlayerEngine.Instance().init(this,true);
+        //ImageLoader.initialize(this, false);
+
+    }
+}
